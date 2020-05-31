aacdbb0179f5bfbf0a5018eddcca9edca3e58af1
kashuvo <shuvo_CSE23@yahoo.com>
2020-05-31 07:26:15 +0600

    Implemented AdminLTE in project

--- source/Applications/tourBD.Web/Startup.cs
+++ source/Applications/tourBD.Web/Startup.cs
@@ -61,6 +61,10 @@ namespace tourBD.Web
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllerRoute(
+                    name: "areas",
+                    pattern: "{area:exists}/{controller=Dashboard}/{action=Index}/{id?}");
+
+                endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
                 endpoints.MapRazorPages();
