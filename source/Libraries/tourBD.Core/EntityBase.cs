2af9665d62ff546577fc6a861ddf4908e938d7b8
kashuvo <shuvo_CSE23@yahoo.com>
2020-06-06 03:41:48 +0600

    Added Autofac for DI

---
+++ source/Libraries/tourBD.Core/EntityBase.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace tourBD.Core
+{
+    public class EntityBase<T> : IEntity<T>
+    {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public T Id { get; set; }
+    }
+}
