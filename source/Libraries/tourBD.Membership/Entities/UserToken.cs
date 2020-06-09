0a815bc82afbdb0945b43a2aa0c4924979df9e4a
kashuvo <shuvo_CSE23@yahoo.com>
2020-06-09 09:47:33 +0600

    Moved Identity & created new migration.

---
+++ source/Libraries/tourBD.Membership/Entities/UserToken.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.AspNetCore.Identity;
+
+namespace tourBD.Membership.Entities
+{
+    public class UserToken
+        : IdentityUserToken<Guid>
+    {
+        public UserToken()
+            : base()
+        {
+
+        }
+    }
+}
---
+++ source/Libraries/tourBD.Membership/MembershipModule.cs
@@ -0,0 +1,30 @@
+﻿using Autofac;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using tourBD.Membership.Contexts;
+
+namespace tourBD.Membership
+{
+    public class MembershipModule : Module
+    {
+        private readonly string _connectionString;
+        private readonly string _migrationAssemblyName;
+
+        public MembershipModule(string connectionString, string migrationAssemblyName)
+        {
+            _connectionString = connectionString;
+            _migrationAssemblyName = migrationAssemblyName;
+        }
+
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterType<ApplicationDbContext>()
+                .WithParameter("connectionString", _connectionString)
+                .WithParameter("migrationAssemblyName", _migrationAssemblyName)
+                .InstancePerLifetimeScope();
+
+            base.Load(builder);
+        }
+    }
+}
