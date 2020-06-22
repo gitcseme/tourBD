d5b1df55959b7fd79c0e8cb89ee170786bc22336
kashuvo <shuvo_CSE23@yahoo.com>
2020-06-22 23:22:10 +0600

    registration with image upload is working

---
+++ source/Applications/tourBD.Web/Models/RegistrationFormModel.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace tourBD.Web.Models
+{
+    public class RegistrationFormModel
+    {
+        public IFormFile ImageFile { get; set; }
+
+        [Required(ErrorMessage = "Name is required")]
+        public string Name { get; set; }
+
+
+        [Required(ErrorMessage = "Email is required")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "You must provide a phone number")]
+        [DataType(DataType.PhoneNumber)]
+        public string Mobile { get; set; }
+
+        public string Address { get; set; }
+    }
+}
