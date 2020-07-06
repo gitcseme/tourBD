08c8fb1932daf6100ee893d668ee5a266e2569bb
kashuvo <shuvo_CSE23@yahoo.com>
2020-07-06 23:11:11 +0600

    Created class library project & entities for Post

---
+++ source/Libraries/tourBD.Forum/Entities/Replay.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using tourBD.Core;
+
+namespace tourBD.Forum.Entities
+{
+    public class Replay : EntityBase<Guid>
+    {
+        public Guid AuthorId { get; set; }
+
+        public Guid CommentId { get; set; }
+
+        public DateTime CreationDate { get; set; }
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Replay can't be empty")]
+        public string Message { get; set; }
+    }
+}
