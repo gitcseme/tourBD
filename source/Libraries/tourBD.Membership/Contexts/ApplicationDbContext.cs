85e89d047e5c720b1f35406de2194614c3b82a89
kashuvo <shuvo_CSE23@yahoo.com>
2020-07-24 12:46:24 +0600

    created company, package, stop entity & relationships

--- source/Libraries/tourBD.Membership/Contexts/ApplicationDbContext.cs
+++ source/Libraries/tourBD.Membership/Contexts/ApplicationDbContext.cs
@@ -35,6 +35,20 @@ namespace tourBD.Membership.Contexts
         protected override void OnModelCreating(ModelBuilder builder)
         {
             base.OnModelCreating(builder);
+
+            builder.Entity<TourPackage>()
+                .HasOne(tp => tp.Company)
+                .WithMany(c => c.TourPackages)
+                .HasForeignKey(tp => tp.CompanyId);
+
+            builder.Entity<Spot>()
+                .HasOne(s => s.TourPackage)
+                .WithMany(tp => tp.Spots)
+                .HasForeignKey(s => s.TourPackageId);
         }
+
+        public DbSet<Company> Companies { get; set; }
+        public DbSet<TourPackage> TourPackages { get; set; }
+        public DbSet<Spot> Spots { get; set; }
     }
 }
---
+++ source/Libraries/tourBD.Membership/Repositories/TourPackageRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using tourBD.Core;
+using tourBD.Membership.Contexts;
+using tourBD.Membership.Entities;
+
+namespace tourBD.Membership.Repositories
+{
+    public class TourPackageRepository : RepositoryBase<TourPackage, Guid, ApplicationDbContext>, ITourPackageRepository
+    {
+        public TourPackageRepository(ApplicationDbContext context) : base(context)
+        {
+        }
+    }
+}
