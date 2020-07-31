1a00ceb7ae79770dc90f04f07bc71d2c8c557c23
kashuvo <shuvo_CSE23@yahoo.com>
2020-07-31 12:18:35 +0600

    Datatable is working

--- source/Applications/tourBD.Web/Areas/Admin/Controllers/DashboardController.cs
+++ source/Applications/tourBD.Web/Areas/Admin/Controllers/DashboardController.cs
@@ -1,9 +1,13 @@
 ﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using tourBD.Core;
+using tourBD.Membership.Entities;
+using tourBD.Membership.Services;
 
 namespace tourBD.Web.Areas.Admin.Controllers
 {
@@ -11,9 +15,52 @@ namespace tourBD.Web.Areas.Admin.Controllers
     [Authorize(Roles = "Admin")]
     public class DashboardController : Controller
     {
+        private readonly ICompanyRequestService _companyRequestService;
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public DashboardController(UserManager<ApplicationUser> userManager, ICompanyRequestService companyRequestService)
+        {
+            _companyRequestService = companyRequestService;
+            _userManager = userManager;
+        }
+
         public IActionResult Index()
         {
             return View();
+        }
+
+        public IActionResult RequestList()
+        {
+
+            return View();
+        }
+
+        public  JsonResult GetRequests()
+        {
+            DatatableAjaxRequestModel request = new DatatableAjaxRequestModel(Request);
+            var requestData = _companyRequestService.GetRequests(request.PageIndex, request.PageSize, true, request.SearchText, "", "");
+
+            var result = new
+            {
+                recordsTotal = requestData.Item2,
+                recordsFiltered = requestData.Item3,
+                data = requestData.Item1.Select(r =>
+                       {
+                           var user = _userManager.FindByIdAsync(r.UserId.ToString()).Result;
+                           string imageUrl = user.ImageUrl;
+
+                           return new string[]
+                           {
+                               imageUrl,
+                               r.Description,
+                               r.RequestDate.ToString(),
+                               r.RequestStatus,
+                               r.Id.ToString()
+                           };
+                       })
+            };
+
+            return Json(result);
         }
     }
 }
