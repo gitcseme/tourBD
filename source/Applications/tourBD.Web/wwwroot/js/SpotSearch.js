a80c23bf9505998e4c5b5a22702335459e605e8c
kashuvo <shuvo_CSE23@yahoo.com>
2020-09-23 00:00:43 +0600

    Google authentication is created.

---
+++ source/Applications/tourBD.Web/wwwroot/js/SpotSearch.js
@@ -0,0 +1,6 @@
+﻿
+$('.card-body .spot-list').on('click', 'div', function (event) {
+    let searchText = event.currentTarget.innerText;
+    let url = 'http://www.google.com/search?q=' + searchText;
+    window.open(url, '_blank');
+});
