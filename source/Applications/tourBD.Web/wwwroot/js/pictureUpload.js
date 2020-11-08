43835726c5b2328f76acffb60295f8274264b170
kashuvo <shuvo_CSE23@yahoo.com>
2020-11-08 19:27:59 +0600

    fixed css & js file & refactored

---
+++ source/Applications/tourBD.Web/wwwroot/js/pictureUpload.js
@@ -0,0 +1,15 @@
+﻿
+$(document).ready(function () {
+    $('#filePhoto').change(function () {
+
+        if (this.files && this.files[0]) {
+            var reader = new FileReader();
+            reader.onload = (e) => {
+                $('#cover-photo').attr('src', e.target.result);
+            };
+
+            reader.readAsDataURL(this.files[0]);
+        }
+
+    })
+});
